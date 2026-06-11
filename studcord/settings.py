db5feb49d2b945d7aa7673e16d9ef0d4601daf3f
amaan29 <amaanwork.p@gmail.com>
2026-06-11 18:14:40 +0530

    fix: comma fix in settings

--- studcord/settings.py
+++ studcord/settings.py
@@ -49,7 +49,7 @@ AUTH_USER_MODEL = 'base.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
